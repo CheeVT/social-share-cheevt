f20a1cde21739c98519669cef68050edcc75b546
Chee <cheevt@gmail.com>
2020-09-27 19:05:58 +0200

    remove unused file

--- public/templates/horizontal.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$shareIcons = '<div class="social-share-horizontal" style="display: flex; ">';
-foreach($this->options['social_networks'] as $network => $enabled) {
-  $shareIcons .= '<a href="#" style="padding: 5px;"><img src="' . SOCIAL_SHARE_CHEEVT_PLUGIN_URL . '/assets/images/' . $network . '" style="width: 30px;"/></a> ';
-}
-$shareIcons .= '</div>';
